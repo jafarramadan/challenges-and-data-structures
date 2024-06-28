d2483ffcd3aaf4deb7c1747bc987cb8d3f28f17b
Ja'far Ramadan <23040271@student.ltuc.com>
2024-06-28 23:10:12 +0300

    Add project files.

---
+++ Find-Duplicates/Find-Duplicates/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Find_Duplicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
