ede6dea3ead393c1dfcba09a35ab4a67f7854eb8
Ja'far Ramadan <23040271@student.ltuc.com>
2024-07-12 03:15:13 +0300

    done !

---
+++ Data Structures/LinkedListProject/LinkedListProject/LinkedList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProject
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int data ) { 
+
+            Data = data;
+            Next = null;
+        }
+    }
+
+
+    public class LinkedList
+    {
+        public Node Head { get; private set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public bool Includes(int data)
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                if (current.Data == data)
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Remove(int data)
+        {
+            if (Head == null)
+            {
+                throw new InvalidOperationException("The list is empty.");
+            }
+
+            if (Head.Data == data)
+            {
+                Head = Head.Next;
+                return;
+            }
+
+            Node current = Head;
+            while (current.Next != null && current.Next.Data != data)
+            {
+                current = current.Next;
+            }
+
+            if (current.Next == null)
+            {
+                throw new InvalidOperationException("Data not found in the list.");
+            }
+
+            current.Next = current.Next.Next;
+        }
+
+        public void PrintList()
+        {
+            Node current = Head;
+            Console.Write("Head -> ");
+            while (current != null)
+            {
+                Console.Write(current.Data + " -> ");
+                current = current.Next;
+            }
+            Console.WriteLine("Null");
+        }
+    }
+}
---
+++ Data Structures/LinkedListProject/LinkedListProject/README.md
@@ -0,0 +1,11 @@
+### Linked List implementaion
+
+
+### Descreption :
+This project contains a basic implementation of a singly linked list in C#. The linked list includes functionalities to add nodes, check for the presence of specific data, remove nodes, and print the list contents. This implementation also comes with unit tests using xUnit.
+
+### whitboard image :
+![Whiteboard Image](assets/LinkedListWB2.PNG)
+
+### Output image:
+![Output Image](assets/LinkedListOP2.PNG)
