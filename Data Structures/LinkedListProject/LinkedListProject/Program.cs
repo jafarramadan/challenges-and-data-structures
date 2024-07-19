44638c4fa557ee7c6c2668f28bb747b9c65e92ed
Ja'far Ramadan <23040271@student.ltuc.com>
2024-07-19 05:34:05 +0300

    done!

--- Data Structures/LinkedListProject/LinkedListProject/Program.cs
+++ Data Structures/LinkedListProject/LinkedListProject/Program.cs
@@ -6,20 +6,33 @@ namespace LinkedListProject
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("===LinkedList implementation===");
             LinkedList list = new LinkedList();
             list.Add(5);
-            list.Add(20);
-            list.Add(20);
             list.Add(10);
-            list.Add(5);
-            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.PrintList();
+            list.Remove(10);
+            Console.WriteLine("After removing element :");
+            list.PrintList();
+            Console.WriteLine("\ndoes the list include 20? " + list.Includes(20)+"\n");
 
+            //========================================================================================================
+            Console.WriteLine("===Remove Duplicate===");
+            LinkedList list2 = new LinkedList();
+            list2.Add(5);
+            list2.Add(20);
+            list2.Add(20);
+            list2.Add(10);
+            list2.Add(5);
+            list2.Add(10);
             Console.WriteLine("Original List:");
-            list.PrintList();
-
+            list2.PrintList();
             Console.WriteLine("\nAfter Removing Duplicates:");
-            list.RemoveDuplicate();
-            list.PrintList();
+            list2.RemoveDuplicate();
+            list2.PrintList();
+            //=========================================================================================================
 
             
             
