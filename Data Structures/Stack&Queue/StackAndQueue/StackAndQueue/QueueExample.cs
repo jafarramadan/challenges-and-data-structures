be11e11f62d375ff41432d06ec9791c402a30218
Ja'far Ramadan <23040271@student.ltuc.com>
2024-07-25 15:15:50 +0300

    StackAndQueue !

---
+++ Data Structures/Stack&Queue/StackAndQueue/StackAndQueue/QueueExample.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class QueueExample
+    {
+        public Node front;
+        public Node back;
+
+        public QueueExample()
+        {
+            front = back = null;
+        }
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (IsEmpty())
+            {
+                front = back = newNode;
+            }
+            else
+            {
+                back.Next = newNode;
+                back = newNode;
+            }
+        }
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Queue is empty");
+            }
+            else
+            {
+               int data  = front.Data;
+                front=front.Next;
+
+                if (IsEmpty())
+                {
+                    back = null;
+                }
+                return data;    
+            }
+        }
+
+        public int Peek()
+        {
+            return front.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == null;
+        }
+    }
+}
