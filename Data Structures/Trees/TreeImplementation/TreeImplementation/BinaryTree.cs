0cc5b7c9b175d077a8fc6e80cbbbbaddfa2c1619
Ja'far Ramadan <23040271@student.ltuc.com>
2024-08-12 19:25:07 +0300

    done!

---
+++ Data Structures/Trees/TreeImplementation/TreeImplementation/BinaryTree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TreeImplementation
+{
+    public class BinaryTree
+    {
+        public TNode Root { get; set; }
+        public BinaryTree(int rootValue)
+        {
+            Root = new TNode(rootValue);
+        }
+
+        public void insert(int value)
+        {
+            insertRecursion(value, Root);
+        }
+
+        private void insertRecursion(int value, TNode node)
+        {
+            if (value < node.Value)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = new TNode(value);
+                }
+                else { insertRecursion(value, node.Left); }
+
+            }
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new TNode(value);
+                }
+                else { insertRecursion(value, node.Right); }
+            }
+
+        }
+
+        //root-left-right
+        public void PreOrder(TNode node)
+        {
+            if (node == null) return;
+            Console.WriteLine(node.Value);
+            PreOrder(node.Left);
+            PreOrder(node.Right);
+
+        }
+
+        //left-root-right
+        public void InOrder(TNode node)
+        {
+            if (node == null) return;
+            InOrder(node.Left);
+            Console.WriteLine(node.Value + "-");
+            InOrder(node.Right);
+        }
+
+        //left-right-root
+        public void PostOrder(TNode node)
+        {
+            if (node == null) return;
+            PostOrder(node.Left);
+            PostOrder(node.Right);
+            Console.WriteLine(node.Value + "-");
+        }
+
+        public void print(TNode node)
+        {
+            Console.WriteLine("----------------------------->");
+            print2DUtil(node, 0);
+        }
+
+        int COUNT = 7;
+        private void print2DUtil(TNode root, int space)
+        {
+            if (root == null)
+                return;
+            space += COUNT;
+            print2DUtil(root.Right, space);
+            Console.Write("\n");
+            for (int i = COUNT; i < space; i++)
+                Console.Write(" ");
+            Console.Write(root.Value + "\n");
+            print2DUtil(root.Left, space);
+        }
+
+
+    }
+}
---
+++ Data Structures/Trees/TreeImplementation/TreeImplementation/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TreeImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BinarySearchTree b = new BinarySearchTree(10);
+            b.Add(5);
+            b.Add(15);
+            b.Add(3);
+            b.Add(7);
+            b.Add(12);
+            b.Add(18);
+
+
+
+            Console.WriteLine(b.Contains(15, b.Root));
+
+        }
+    }
+}
