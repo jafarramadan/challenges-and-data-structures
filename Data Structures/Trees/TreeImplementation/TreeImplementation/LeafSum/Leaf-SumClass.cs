cee22b1c61d79aff397514c62f04ea037bd5a237
Ja'far Ramadan <23040271@student.ltuc.com>
2024-08-22 18:33:23 +0300

    LeafSum!

---
+++ Data Structures/Trees/TreeImplementation/TreeImplementation/LeafSum/Leaf-SumClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeImplementation.TreeImplementation;
+
+namespace TreeImplementation.LeafSum
+{
+    public class Leaf_SumClass 
+    {
+        public int SumLeaf(TNode node)
+        {
+            if (node == null) return 0;
+            if (node.Left == null && node.Right == null) return node.Value;
+            return  SumLeaf(node.Left) + SumLeaf(node.Right);
+
+        }
+    }
+}
